844fcfb321f66ce99eb42177e5c6fdab35ee57b7
Glimpz <abgabbard@gmail.com>
2021-04-07 18:03:02 -0700

    added code to console log axios result from marvel API

--- tierspractice/src/App.js
+++ tierspractice/src/App.js
@@ -1,7 +1,15 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import API from './utils/API';
 
 function App() {
+
+const [characters, setCharacters] = useState([]); 
+  useEffect(() => {
+    API.search().then(result => {
+      console.log(result.data.data.results)
+    })
+  }, [])
   return (
     <div>
 
--- tierspractice/src/utils/API.js
+++ tierspractice/src/utils/API.js
@@ -6,7 +6,7 @@ const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
 const marvelURL = "http://gateway.marvel.com/v1/public/characters?ts=1&limit=50&apikey=3c14f22cccc40f2a5fe8c84b010ca86c&hash=6fac2ceafd3cb12c55fe7b17df6ff4d6"
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  search: function(query) {
+  search: function() {
     return axios.get(marvelURL);
   }
 };
