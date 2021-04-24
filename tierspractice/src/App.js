e9bbc92679e44c67c869752d60bfca49b134fd38
Glimpz <abgabbard@gmail.com>
2021-04-24 07:30:40 -0700

    adding another sort (name)

--- tierspractice/src/App.js
+++ tierspractice/src/App.js
@@ -1,47 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Character from './components/Character';
-import API from './utils/API';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Character from "./components/Character";
+import API from "./utils/API";
 
 function App() {
-
-const [characters, setCharacters] = useState([]);
-const [sortAscending, setSortAscending] = useState(true) 
+  const [characters, setCharacters] = useState([]);
+  const [sortAscending, setSortAscending] = useState(true);
+  const [sortName, setSortName] = useState('name');
 
   useEffect(() => {
-    API.search().then(myResults => {
-     setCharacters(myResults.data.data.results
-      .map( character => {
-       return {
-        id: character.id, 
-        name: character.name,
-        image: character.thumbnail.path + '.' + character.thumbnail.extension
-       }
-     })
-     .sort((a,b) => b.name.localeCompare(a.name))
-     )
-    })
-  }, [])
+    API.search().then((myResults) => {
+      setCharacters(
+        myResults.data.data.results
+          .map((character) => {
+            return {
+              id: character.id,
+              name: character.name,
+              image:
+                character.thumbnail.path + "." + character.thumbnail.extension,
+            };
+          })
+          .sort((a, b) => b.name.localeCompare(a.name))
+      );
+    });
+  }, []);
   const handlesort = () => {
-setSortAscending(!sortAscending)
-  }
-  console.log("characters:", characters)
+    setSortAscending(!sortAscending);
+  };
+  console.log("characters:", characters);
   return (
     <div>
-       <button onClick={handlesort}>Toggle Sort</button>
-        { characters
-        .sort((a,b) => {
+      <button onClick={handlesort}>Toggle Sort</button>
+      {characters
+        .sort((a, b) => {
           if (sortAscending) {
-            return a.name.localeCompare(b.name) 
+            return a[sortName].localeCompare(b[sortName])
+          } else {
+            return b[sortName].localeCompare(a[sortName]);
           }
-            else {
-              return b.name.localeCompare(a.name)
-            }
-          }
-        )
-        .map(c => <Character name={c.name} id={c.id} image={c.image}/>)}
-       
-    </ div>
+        })
+        .map((c) => (
+          <Character name={c.name} id={c.id} image={c.image} />
+        ))}
+    </div>
   );
 }
 
