73ceef3729096b9eca206fb784eab0622eb60efc
Glimpz <abgabbard@gmail.com>
2021-04-23 21:23:24 -0700

    updated margin and style width

--- tierspractice/src/components/Character/index.js
+++ tierspractice/src/components/Character/index.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Character = (props) => {
     return (
         
-        <div styl={{marginBottom: 10}}>
+        <div style={{marginBottom: 40}}>
             <h4>
             {props.name}
             </h4>
-        <img src={props.image} alt={props.name}></img>
+        <img src={props.image} alt={props.name} style={{maxWidth: 300}}></img>
         </div>
     )
 }
